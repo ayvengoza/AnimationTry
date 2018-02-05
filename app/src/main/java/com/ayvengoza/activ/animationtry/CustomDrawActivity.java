d4c62bf07f3f28aa81c14bd63c3864f2ec67a735
Andrii Zastupailo <a.zastupailo@samsung.com>
2018-02-06 00:41:26 +0200

    add parameter
    Signed-off-by: andriizastupailo <ayvengoza@gmail.com>

--- app/src/main/java/com/ayvengoza/activ/animationtry/CustomDrawActivity.java
+++ app/src/main/java/com/ayvengoza/activ/animationtry/CustomDrawActivity.java
@@ -2,10 +2,17 @@ package com.ayvengoza.activ.animationtry;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by andriizastupailo on 04.02.18.
@@ -19,6 +26,38 @@ public class CustomDrawActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_draw);
         mConstraintLayout = (ConstraintLayout)findViewById(R.id.custom_text);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_delete);
+        String parameters = String.format("%s %s", Build.MODEL, Build.VERSION.RELEASE);
+        String androidID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        String macAddress = getMacAddr();
+        Log.d("Test string", parameters);
+        Log.d("Test id", macAddress);
+
         mConstraintLayout.setBackground(new SimpleImageDrawable(bitmap));
+    }
+
+    public static String getMacAddr() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:",b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+        }
+        return "02:00:00:00:00:00";
     }
 }
